fb90286e2aab09c9e85a0ffb49a7e883248a1291
DiegoPullotasig <diego.pullotasig@epn.edu.ec>
2019-10-17 17:53:45 -0500

    clase 17-10-2019
    primitivas

---
+++ leguajes/01-variables.js
@@ -0,0 +1,35 @@
+/* 
+Nombre: Diego Pullotasig
+Fecha actualizacion: 17-10-2019
+Descripcion: Ejemplos de variables primitivas en js
+
+SINTAXIS DEL PROGRAMA
+*/
+
+// comentario
+/* <--- abro bloque comentario
+
+cierro bloque comentario --> */ 
+
+// Primitivas (tipos de variables)
+/*
+int           --> Numero ENTERO     Ej: 5, -3, 0 
+char          --> Caracter          Ej: a, b, c      
+float         --> Numero Decimal    Ej: 12.12, -14.34, 3.03          
+double        --> Numero Decimal ++ Ej: 12.12121...
+string        --> Arreglo de char   Ej: "abc", "qwe"        
+null          --> Vacio             Ej: null   
+boolean       --> Verdadero o Falso Ej: true o false   
+*/
+
+// Palabras reservadas
+// var edad = 30;           --> No se usa
+// let edad = 30;    // Mutable   --> Cambia
+const edad = 30;  // Inmutable --> No Cambia
+// Ejemplo
+const IVA = 12;
+// IVA = 14; // No reasignar valores
+let sueldo = 0;
+// edad = 30  --> python
+// var edad: integer = 30;  --> kotlin - Mutable
+// val edad: Integer = 30;  --> kotlin - Inmutable
---
+++ leguajes/02-variables-ts.ts
@@ -0,0 +1,5 @@
+var edad = 30;         
+const IVA = 12;
+let sueldo = 0;
+edad = 30  
+var edad: 
