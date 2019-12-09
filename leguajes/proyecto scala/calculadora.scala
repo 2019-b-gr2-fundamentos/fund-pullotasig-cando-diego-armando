ac53f56c444d73a5b53709b3eb575efba39803b0
diego <diego.pullotasig@epn.edu.ec>
2019-12-09 15:56:12 -0500

    calculadora escala

---
+++ leguajes/proyecto scala/calculadora.scala
@@ -0,0 +1,117 @@
+object calculadora{ // pedir valor y guardamos en operaciones
+    val operacion = 6
+    println(operacion match{
+        case 1 => {
+            def suma(A: Int, B: Int):Int{
+                A+B
+            }
+        } 
+        case 2 => {
+            def resta(A: Int, B: Int):Int{
+                A-B
+            }
+        } 
+        case 3 => {
+            def division(A: Int, B: Int):Int{
+                A/B
+            }
+        }
+        case 4 => {
+            def multiplicacion(A: Int, B: Int):Int{
+                A*B
+            }
+        }
+        case 5 => {
+            def cuadrado(A: Int, B: Int):Int{
+                var resultado = math.pow(A,B)
+                println("el resultado es" + resultado)
+            }
+        }
+        case 6 => {
+            def suma(A: Int, B: Int):Int{
+                A+B
+            }
+        } 
+        case 7 => {
+            def suma(A: Int, B: Int):Int{
+                A+B
+            }
+        }
+        case 8 => {
+            def suma(A: Int, B: Int):Int{
+                A+B
+            }
+        } 
+        case 9 => {
+            def suma(A: Int, B: Int):Int{
+                A+B
+            }
+        }
+        case 10 => {
+            def suma(A: Int, B: Int):Int{
+                A+B
+            }
+        }
+        case _ => {
+            def suma(A: Int, B: Int):Int{
+                A+B
+            }
+        }
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+object calaculadora {
+    def suma(valorA: Int, valorB: Int):Int ={
+        var valor1 = new scala
+    }
+    def resta(valorA: Int, valorB: Int):Int {
+
+    }                              
+    def division(valorA: Int, valorB: Int):Int {
+
+    }                                           
+    def multiplicacion(valorA: Int, valorB: Int):Int {
+
+    }
+    def cuadrado(valorA: int, )
+
+
+
+
+}
