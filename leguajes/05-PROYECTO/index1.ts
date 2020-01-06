b695ae0b1e5b722c793352bd6ef5138537122109
diego <diego.pullotasig@epn.edu.ec>
2020-01-06 16:07:05 -0500

    librerias

---
+++ leguajes/05-PROYECTO/index1.ts
@@ -0,0 +1,7 @@
+// node modules
+//const sumarLibreria = require('diego-armandofund/lib/suma');
+//Importar todo el contenido con el nombre 'suamrLibreria' del paquete 'diego-armandofund/lib/suma'
+import * as sumarLibreria from 'diego-armandofund/lib/suma';
+
+const respuesta = sumarLibreria(1,3);
+console.log('La respuesta es:', respuesta);
