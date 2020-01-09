4f181cb9d4cc6dbbcf06a693bf092905cbd9fa15
diego pullotasig <diego.pullotasig@epn.edu.ec>
2020-01-09 16:34:38 -0500

    libreria

--- leguajes/05-PROYECTO/index1.ts
+++ leguajes/05-PROYECTO/index1.ts
@@ -2,6 +2,8 @@
 //const sumarLibreria = require('diego-armandofund/lib/suma');
 //Importar todo el contenido con el nombre 'suamrLibreria' del paquete 'diego-armandofund/lib/suma'
 import * as sumarLibreria from 'diego-armandofund/lib/suma';
+import * as PI from 'diego-armandofund/lib/pi';
 
 const respuesta = sumarLibreria(1,3);
 console.log('La respuesta es:', respuesta);
+console.log('la constante pi es:',PI)
